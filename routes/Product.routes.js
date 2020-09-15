22fe997e8472e82c08a55c76552c5fcfbd806dc3
eckzen <eckzen@yahoo.com>
2020-09-15 17:21:20 +0800

    Backend Ready

--- routes/Product.routes.js
+++ routes/Product.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const Product = require("../Models/Product.model");
 const ProductController = require("../Controllers/Product.controllers");
 
 // Gett all product list
